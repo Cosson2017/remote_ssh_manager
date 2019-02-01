bd35d7d5881bf54b2a435406791ecfb93883f8e3
Cosson2017 <20379044+Cosson2017@users.noreply.github.com>
2019-02-01 08:27:06 +0800

    update

--- src/rsm_tui.rs
+++ src/rsm_tui.rs
@@ -1,89 +1,64 @@
-use std::io;
-//use std::boxed::FnBox;
-
-extern crate termion;
-extern crate tui;
-extern crate failure;
-
-use termion::event::Key;
-use termion::input::MouseTerminal;
-use termion::raw::IntoRawMode;
-use termion::raw::RawTerminal;
-use termion::screen::AlternateScreen;
-use tui::backend::TermionBackend;
-use tui::layout::{Constraint, Direction, Layout, Rect};
-use tui::style::{Color, Style};
-use tui::widgets::{Block, Borders, List, Paragraph, Text, Widget};
-use tui::Terminal;
-
-// 默认terminal
-type TermionTerm = Terminal<TermionBackend<AlternateScreen<MouseTerminal<RawTerminal<io::Stdout>>>>>;
-
-pub enum InteractiveMode {
-    MAIN,
-    SEARCH,
-    DELETE,
-    ADD,
-    UPDDATE,
+trait Model {
+    fn get_line(&self) -> Vec<&str>;
 }
 
-pub struct App {
-    mode: InteractiveMode,
-    size: Rect,
-    terminal: TermionTerm,
+trait Widget {
+    fn draw(&self);
+    fn event(&mut self, key: char);
 }
 
-impl Default for App {
-    fn default() -> App {
-        App {
-            mode: InteractiveMode::MAIN,
-            size: Rect::default(),
-            terminal: App::init().unwrap(),
+enum State {
+    Normal,
+    Search(String, String), // (left text,right text)
+}
+
+struct MainWidget {
+    state: State,
+}
+
+impl Widget for MainWidget {
+    fn draw(&self) {}
+
+    fn event(&mut self, key: char) {
+        use State::*;
+        match self.state {
+            Normal => self.event_normal(key),
+            Search(..) => self.evnet_search(key),
         }
     }
 }
 
-impl App {
-    pub fn init() -> Result<TermionTerm, failure::Error> {
-        let stdout = io::stdout().into_raw_mode()?;
-        let stdout = MouseTerminal::from(stdout);
-        let stdout = AlternateScreen::from(stdout);
-        let backend = TermionBackend::new(stdout);
-        let mut terminal = Terminal::new(backend)?;
-        terminal.hide_cursor()?;
-        Ok(terminal)
+impl MainWidget {
+    fn new() -> Self {
+        Self {
+            state: State::Normal,
+        }
+    }
+    fn event_normal(&mut self, key: char) {
+        use State::*;
+        match key {
+            '/' => self.state = Search("/".into(), "".into()),
+            '\t' => {}
+            _ => {}
+        }
     }
 
-    // 主ui
-    pub fn main_ui(&mut self) {
-        self.terminal.draw(|mut f|{
-
-        });
-    }
-
-    // 搜索ui
-    pub fn search_ui(&self) -> Box<FnOnce(TermionTerm)> {
-        Box::new(|mut f: TermionTerm| {})
-    }
-
-    // 删除ui
-    pub fn delete_ui(&self) {
-
-    }
-
-    // 添加ui
-    pub fn add_ui(&self) {
-
-    }
-
-    // 更新ui
-    pub fn update_ui(&self) {
-
-    }
-
-    // key event
-    pub fn key_event() {
-
+    fn evnet_search(&mut self, key: char) {
+        use State::*;
+        match key {
+            '\n' => {}
+            '\t' => {}
+            '\x1B' => {
+                //'esc'
+            }
+            // '->' => {}
+            // '<-' => {}
+            // 'esc' => {}
+            _ => {
+                if let Search(ref mut front, ..) = self.state {
+                    front.push(key);
+                }
+            }
+        }
     }
 }
-
